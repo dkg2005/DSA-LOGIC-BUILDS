2ddfa45c57fb008326cbb51afaf1ce6bce569b4c
dkg2005 <dhruvgupta766671@gmail.com>
2024-07-19 19:31:34 +0530

    tree learning

--- TREE/binarytree.cpp
+++ TREE/binarytree.cpp
@@ -1,4 +1,5 @@
 #include<iostream>
+#include<queue>
 using namespace std;
 
 class node{
@@ -10,7 +11,6 @@ class node{
 
     public:
     node(int data){
-        cout<<"A new node is declared "<<endl;
         this->data = data;
         this->left = NULL;
         this->right = NULL;
@@ -19,7 +19,7 @@ class node{
 
 node* buildTree(node* root){
     
-    cout<<"Enter the data which you want to insert : " <<endl;
+    cout<<"Enter the data : " <<endl;
     int data;
     cin >> data;
     root = new node(data);
@@ -28,20 +28,140 @@ node* buildTree(node* root){
         return NULL;
     }
 
-    cout<<"Enter the data which you want to insert in the left :"<< data <<endl;
+    cout<<"Enter the data which you want to insert in the left of"<< data <<endl;
     root->left = buildTree(root->left);
-    cout<<"Enter the data which you want to insert in the right : "<< data <<endl;
+    cout<<"Enter the data which you want to insert in the right of "<< data <<endl;
     root->right = buildTree(root->right);
 
   return root;
 }
 
+ void levelOrderTraversal(node* root){
+    if(root == NULL){
+        cout<<"Root is Empty"<<endl;
+        return;
+    }
+     queue<node*> q;
+
+     q.push(root);
+     q.push(NULL);   // LEVEL DELIMITER
+
+     while( !q.empty() ){
+
+         node * temp = q.front();
+         q.pop();
+
+         if( temp == NULL ){
+            // means old node has complety.
+            cout<<endl;
+            
+            if( !q.empty() ){
+                // queue has some still child nodes
+                q.push(NULL);
+            }
+         }
+
+         else{
+               cout<< temp -> data <<" " ;
+
+               if(temp -> left){
+                    q.push(temp -> left);
+               }
+
+               if(temp -> right){
+                   q.push(temp -> right);
+               }
+         }
+     }
+ }
+
+ void inorderTraversal(node* root){
+    // [ LRN ]
+    if(root == NULL){
+        return ;
+    }
+     
+     inorderTraversal(root -> left);     
+     cout<< root -> data << " ";
+     inorderTraversal(root -> right);     
+ }
+
+   void preorderTraversal(node* root){
+    // [ NLR ]
+    if(root == NULL){
+        return ;
+    }
+
+     cout<< root -> data << " ";
+     preorderTraversal(root -> left);     
+     preorderTraversal(root -> right);     
+ }
+  
+   void postorderTraversal(node* root){
+    // [ LRN ]
+    if(root == NULL){
+        return ;
+    }
+     
+     postorderTraversal(root -> left);       
+     postorderTraversal(root -> right);    
+     cout<< root -> data << " "; 
+ }
+
+  void buildfromLevelOrder(node* &root){
+     queue<node*>q;
+
+     cout<<"Enter the data of root node: "<<endl;
+     int data;
+     cin>> data;
+     root = new node(data);
+     q.push(root);
+
+     while(!q.empty()){
+        node* temp = q.front();
+        q.pop();
+
+           cout<<"Enter the left node data of :" << temp->data <<endl;
+           int leftData;
+           cin>>leftData;
+
+            if(leftData != -1) {
+               temp -> left = new node(leftData);
+                q.push(temp->left);
+            }
+
+           cout<<"Enter the right node data of :" << temp->data <<endl;
+           int rightData;
+           cin>>rightData;
+   
+           if(rightData != -1) {
+                temp -> right = new node(rightData);
+                q.push(temp->right);
+            }
+
+     }
+  }
 
 int main(){
 
     node* root = NULL;
-    buildTree(root);
-    
+   //   root = buildTree(root);
+
+    // 1 2 4 -1 -1 5 -1 -1 3 7 -1 -1 8 -1 -1 -1 -1 -1
+
+    buildfromLevelOrder(root);
+    levelOrderTraversal(root);
+
+
+    //   cout << endl << "The inOrderTraversal of the tree is :  " ;
+    //   inorderTraversal(root);
+
+    //   cout << endl << "The preOrderTraversal of the tree is :  " ;
+    //   preorderTraversal(root);
+
+    //   cout << endl << "The postOrderTraversal of the tree is :  " ;
+    //   postorderTraversal(root);
+
 
     return 0;
 }
--- TREE/testcaseforbuildatree.md
+++ TREE/testcaseforbuildatree.md
@@ -59,3 +59,122 @@ Enter the data which you want to insert in the right : 8
 Enter the data which you want to insert :
 -1
 A new node is declared
+
+## testcase for levelOrderTraversal() fxn:
+
+
+Enter the data : 
+1 2 4 -1 -1 5 -1 -1 3 7 -1 -1 8 -1 -1 -1 -1 -1
+Enter the data which you want to insert in the left of1
+Enter the data : 
+Enter the data which you want to insert in the left of2
+Enter the data : 
+Enter the data which you want to insert in the left of4
+Enter the data : 
+Enter the data which you want to insert in the right of 4
+Enter the data : 
+Enter the data which you want to insert in the right of 2
+Enter the data : 
+Enter the data which you want to insert in the left of5
+Enter the data : 
+Enter the data which you want to insert in the right of 5
+Enter the data :
+Enter the data which you want to insert in the right of 1
+Enter the data :
+Enter the data which you want to insert in the left of3
+Enter the data :
+Enter the data which you want to insert in the left of7
+Enter the data :
+Enter the data which you want to insert in the right of 7
+Enter the data :
+Enter the data which you want to insert in the right of 3
+Enter the data :
+Enter the data which you want to insert in the left of8
+Enter the data :
+Enter the data which you want to insert in the right of 8
+Enter the data :
+1
+2 3
+4 5 7 8
+
+
+## test case for the in,pre,post order traversal fxn:
+ 
+ Enter the data : 
+1        
+Enter the data which you want to insert in the left of1
+Enter the data : 
+3  
+Enter the data which you want to insert in the left of3
+Enter the data : 
+7
+Enter the data which you want to insert in the left of7
+Enter the data : 
+-1
+Enter the data which you want to insert in the right of 7
+Enter the data : 
+-1
+Enter the data which you want to insert in the right of 3
+Enter the data : 
+11
+Enter the data which you want to insert in the left of11
+Enter the data :
+-1
+Enter the data which you want to insert in the right of 11
+Enter the data :
+-1
+Enter the data which you want to insert in the right of 1
+Enter the data :
+5
+Enter the data which you want to insert in the left of5
+Enter the data :
+17
+Enter the data which you want to insert in the left of17
+Enter the data :
+-1
+Enter the data which you want to insert in the right of 17
+Enter the data :
+-1
+Enter the data which you want to insert in the right of 5
+Enter the data :
+-1
+1
+3 5
+7 11 17
+- The inOrderTraversal of the tree is :  7 3 11 1 17 5 
+- The preOrderTraversal of the tree is :  1 3 7 11 5 17 
+- The postOrderTraversal of the tree is :  7 11 3 17 5 1
+
+
+
+## test case perform for the function buildfromlevelOrder :
+
+Enter the data of root node: 
+1
+Enter the left node data of :1
+3
+Enter the right node data of :1
+5
+Enter the left node data of :3
+7
+Enter the right node data of :3
+11
+Enter the left node data of :5
+17
+Enter the right node data of :5
+-1
+Enter the left node data of :7
+-1
+Enter the right node data of :7
+-1
+Enter the left node data of :11
+-1
+Enter the right node data of :11
+-1
+Enter the left node data of :17
+-1
+Enter the right node data of :17
+-1
+1 
+3 5
+7 11 17
