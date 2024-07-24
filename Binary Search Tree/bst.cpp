8573eb38ad9cf715aac9afdbe6d97e0f87095826
dkg2005 <dhruvgupta766671@gmail.com>
2024-07-24 16:51:38 +0530

    Completed

--- Binary Search Tree/bst.cpp
+++ Binary Search Tree/bst.cpp
@@ -82,6 +82,73 @@ class Node{
    }
  }
 
+  Node* minVal(Node* root){
+    Node* temp = root;
+
+    while(temp->left != NULL){
+        temp = temp -> left;
+    }
+    return temp;
+  }
+
+  Node* maxVal(Node* root){
+    Node* temp = root;
+
+    while(temp->right != NULL){
+        temp = temp -> right;
+    }
+    return temp;
+  }
+
+  Node* deleteFromBST(Node* root, int val){
+      
+      if(root == NULL)
+        return  root;
+
+    if(root->data == val){
+        // 0 child handle
+        if(root->left == NULL && root->right == NULL){
+            delete root;
+            return NULL;
+        }
+
+        //1 child handle
+
+        //left child
+        if(root->left != NULL && root->right == NULL){
+            Node* temp = root->left;
+            delete root;
+            return temp;
+        }
+
+        //right child
+        if(root->left == NULL && root->right != NULL){
+            Node* temp = root->right;
+            delete root;
+            return temp;
+        }
+ 
+        //2 child handle
+         if(root->left != NULL && root->right != NULL){
+            int mini = minVal(root->right)->data;
+            root->data = mini;
+           root->right =  deleteFromBST(root->right, mini);
+            return root;
+         }
+ 
+    }
+     
+    else if(root->data > val){
+        root->left = deleteFromBST(root->left,val);
+        return root;
+    }
+      
+    else{
+        root->right = deleteFromBST(root->right,val);
+        return root;
+    }
+  }
+
 int main(){
 
   Node* Root = NULL;
@@ -90,10 +157,26 @@ int main(){
 
     takeInput(Root);
 
-   cout<<"printing the Bst: "<< endl;
+    cout<<"printing the Bst: "<< endl;
     levelOrderTraversal(Root);
 
-
+    cout<<endl;
     // HERE WE CAN ALSO TRAVERSE IT BY IN,PRE,POST TRAVERSAL
+
+//    Node* temp = minVal(Root);
+//    cout<<"Minimum value is: "<<temp->data<<endl;
+
+//     temp = maxVal(Root);
+//    cout<<"Maximum value is: "<<temp->data<<endl;
+
+
+  //    Root = deleteFromBST(Root, 30);
+
+   Root = deleteFromBST(Root, 50);
+      
+        cout<<"printing the Bst: "<< endl;
+          levelOrderTraversal(Root);
+
+   
     return 0;
 }
